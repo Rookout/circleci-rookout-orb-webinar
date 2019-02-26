42b3fb3004ecad0f6a1838051db7cf9439da20a8
Itiel <itielshw@gmail.com>
2019-02-26 17:00:07 +0200

    second commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 //server.js
 const app = require('./app')
+const rook = require('rookout');
 
+rook.start({
+    token: process.env.ROOKOUT_TOKEN,
+    tags: ['adding-rookout-to-server']
+})
 app.listen(5678, () => {
     console.log('Example app listening on port http://localhost:5678!');
 })
